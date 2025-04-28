19108881b69464436ca26696bb21e9fec6e86d61
Nikos Alexandropoulos <nalex@Nikoss-MacBook-Pro.local>
2025-04-28 20:30:15 +0300

    Text change

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,8 +7,7 @@ export default function Home() {
       {/* Main Invitation */}
       <div className="max-w-4xl w-full px-4 md:px-8 py-8 relative invitation-container">
         {/* Decorative elements - positioned according to original design */}
-        {/* <div className="absolute top-0 right-0 m-4"> */}
-        <div className="absolute top-10 right-30 z-15">
+        <div className="absolute top-0 right-0 m-4">
           <Image
             src="/images/monkey.png"
             alt="Decorative monkey"
@@ -45,7 +44,7 @@ export default function Home() {
         {/* Invitation Text - now with more spacing and slightly larger */}
         <div className="invitation-text text-2xl md:text-3xl lg:text-4xl mb-12 mt-12 pt-6">
           ελατε να γιορτασουMε την<br />
-          βαπτιση του γιου Μασ !<br />
+          βαπτιση του γιου Μασ!<br />
           σασ περιΜενουΜε την κυριακη 29 ιουνιου 2025 &<br />
           ωρα 11:00Π.Μ. στον ιερο ναο<br />
           αγιου αθανασιου στο πολυδροσο χαλανδριου<br />
